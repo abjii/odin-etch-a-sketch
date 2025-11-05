9c225e5a6a9fd6da81568ededcec222185b89a66
abjii <156369372+abjii@users.noreply.github.com>
2025-11-05 17:44:43 +0100

    make grid

--- eas_script.js
+++ eas_script.js
@@ -1,5 +1,15 @@
+let perRowBoxes = 120;
 const container = document.getElementById("container");
-for (let i = 0; i < 16; i++) {
-  const div = document.createElement("div");
-  container.appendChild(div);
+const containerHeight = container.clientHeight;
+const containerWidth = container.clientWidth;
+
+boxWidth = containerWidth / perRowBoxes;
+boxHeight = containerHeight / perRowBoxes;
+for (let i = 0; i < perRowBoxes; i++) {
+  for (let j = 0; j < perRowBoxes; j++) {
+    const div = document.createElement("div");
+    div.style.width = boxWidth + "px";
+    div.style.height = boxHeight + "px";
+    container.appendChild(div);
+  }
 }
